80b26bc91913fbd5cfc00c5cd0d8706e94e6f1aa
Bernapai <juanemeunier@gmail.com>
2025-04-17 20:21:29 -0300

    Adoption Skeleton/Service Finished

--- Microservicios/adoption-service/src/app.module.ts
+++ Microservicios/adoption-service/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdoptionModule } from './adoption/adoption.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdoptionModule, DatabaseModule],
 })
-export class AppModule {}
+export class AppModule { } }
