612b6f9df9e10a0fc2a66ab9d38b4e37ea66a3fe
Bernapai <juanemeunier@gmail.com>
2025-04-21 18:23:06 -0300

    Add UserDtos

--- Microservicios/user-service/src/main.ts
+++ Microservicios/user-service/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions } from '@nestjs/microservices';
-import { Transport } from '@nestjs/microservices/enums';
+import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -10,13 +11,22 @@ async function bootstrap() {
       transport: Transport.TCP,
       options: {
         host: 'localhost',
-        port: 3005,
+        port: 3004,
       },
     },
   );
 
+  // Añade esto para habilitar la validación
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen();
-  console.log('Microservicio escuchando en el puerto 3001');
+  console.log('Microservicio escuchando en el puerto 3004');
 }
 
 bootstrap().catch((error) => {
