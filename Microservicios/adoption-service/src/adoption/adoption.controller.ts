22b64337ab07189fa9a780799d2085531fc6e456
Bernapai <juanemeunier@gmail.com>
2025-04-23 19:53:37 -0300

    Add adopcionControllers

--- Microservicios/adoption-service/src/adoption/adoption.controller.ts
+++ Microservicios/adoption-service/src/adoption/adoption.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Post,
+} from '@nestjs/common';
+import { AdoptionService } from './adoption.service';
+import { CreateAdopcionDto } from './dto/createAdoption.dto';
+import { UpdateAdopcionDto } from './dto/updateAdoption.dto';
+import { Adopcion } from './adoption.entity';
 @Controller('adoption')
-export class AdoptionController {}
+export class AdoptionController {
+  constructor(private readonly adoptionService: AdoptionService) { }
+
+  @Post()
+  async create(
+    @Body() createAdopcionDto: CreateAdopcionDto,
+  ): Promise<Adopcion> {
+    return this.adoptionService.create(createAdopcionDto);
+  }
+  @Get()
+  async findAll(): Promise<Adopcion[]> {
+    return this.adoptionService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Adopcion> {
+    return this.adoptionService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateAdopcionDto: UpdateAdopcionDto,
+  ): Promise<Adopcion> {
+    return this.adoptionService.update(Number(id), updateAdopcionDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.adoptionService.remove(Number(id));
+  }
+}
