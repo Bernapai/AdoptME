e11b649d140ccccb24c064ced9c3be217ba1c127
Bernapai <juanemeunier@gmail.com>
2025-04-23 19:49:53 -0300

    Add adopcionServices

--- Microservicios/adoption-service/src/adoption/adoption.service.ts
+++ Microservicios/adoption-service/src/adoption/adoption.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Adopcion } from './adoption.entity';
+import { CreateAdopcionDto } from './dto/createAdoption.dto';
+import { UpdateAdopcionDto } from './dto/updateAdoption.dto';
 @Injectable()
-export class AdoptionService {}
+export class AdoptionService {
+  constructor(
+    @InjectRepository(Adopcion)
+    private readonly adopcionRepository: Repository<Adopcion>,
+  ) { }
+
+  async create(createAdopcionDto: CreateAdopcionDto): Promise<Adopcion> {
+    const adopcion = this.adopcionRepository.create(createAdopcionDto);
+    return this.adopcionRepository.save(adopcion);
+  }
+
+  async findAll(): Promise<Adopcion[]> {
+    return this.adopcionRepository.find();
+  }
+
+  async findOne(id: number): Promise<Adopcion> {
+    const adopcion = await this.adopcionRepository.findOneBy({
+      idAdopcion: id.toString(),
+    });
+    if (!adopcion) {
+      throw new Error(`Adopcion with id ${id} not found`);
+    }
+    return adopcion;
+  }
+
+  async update(
+    id: number,
+    updateAdopcionDto: UpdateAdopcionDto,
+  ): Promise<Adopcion> {
+    await this.adopcionRepository.update(id, updateAdopcionDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.adopcionRepository.delete(id);
+  }
+}
