723886ae6b8716053b8821337f1a03cf8d142cb3
Bernapai <juanemeunier@gmail.com>
2025-04-24 08:52:43 -0300

    Add animalControllers

--- Microservicios/animal-service/src/animal/animal.controller.ts
+++ Microservicios/animal-service/src/animal/animal.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Post,
+} from '@nestjs/common';
+import { AnimalService } from './animal.service';
+import { CreateAnimalDomesticoDto } from './dto/createAnimal.dto';
+import { UpdateAnimalDomesticoDto } from './dto/updateAnimal.dto';
+import { AnimalDomestico } from './animal.entity';
 
 @Controller('animal')
-export class AnimalController {}
+export class AnimalController {
+  constructor(private readonly animalService: AnimalService) { }
+
+  @Post()
+  async create(
+    @Body() createAnimalDomesticoDto: CreateAnimalDomesticoDto,
+  ): Promise<AnimalDomestico> {
+    return this.animalService.create(createAnimalDomesticoDto);
+  }
+
+  @Get()
+  async findAll(): Promise<AnimalDomestico[]> {
+    return this.animalService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<AnimalDomestico> {
+    return this.animalService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateAnimalDomesticoDto: UpdateAnimalDomesticoDto,
+  ): Promise<AnimalDomestico> {
+    return this.animalService.update(Number(id), updateAnimalDomesticoDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.animalService.remove(Number(id));
+  }
+}
