ec4cbe5e534ddc36a1133e98598170383db114e5
Bernapai <juanemeunier@gmail.com>
2025-04-25 17:22:04 -0300

    Add appointmentService

--- Microservicios/appointment-service/src/appointment/appointment.service.ts
+++ Microservicios/appointment-service/src/appointment/appointment.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Appointment } from './appointment.entity';
+import { CreateAppointmentDto } from './dto/createAppointment.dto';
+import { UpdateAppointmentDto } from './dto/updateAppointment.dto';
 
 @Injectable()
-export class AppointmentService {}
+export class AppointmentService {
+  constructor(
+    @InjectRepository(Appointment)
+    private readonly appointmentRepository: Repository<Appointment>,
+  ) { }
+
+  async create(
+    createAppointmentDto: CreateAppointmentDto,
+  ): Promise<Appointment> {
+    const appointment = this.appointmentRepository.create(createAppointmentDto);
+    return this.appointmentRepository.save(appointment);
+  }
+
+  async findAll(): Promise<Appointment[]> {
+    return this.appointmentRepository.find();
+  }
+
+  async findOne(id: number): Promise<Appointment> {
+    const appointment = await this.appointmentRepository.findOneBy({
+      idCita: id.toString(),
+    });
+    if (!appointment) {
+      throw new Error(`Appointment with id ${id} not found`);
+    }
+    return appointment;
+  }
+
+  async update(
+    id: number,
+    updateAppointmentDto: UpdateAppointmentDto,
+  ): Promise<Appointment> {
+    await this.appointmentRepository.update(id, updateAppointmentDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.appointmentRepository.delete(id);
+  }
+}
