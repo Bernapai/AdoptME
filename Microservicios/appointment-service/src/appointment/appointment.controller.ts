7c711bb5e515e2d22e8c751e227596d2315f7495
Bernapai <juanemeunier@gmail.com>
2025-04-25 17:23:55 -0300

    Add appointkmentController

--- Microservicios/appointment-service/src/appointment/appointment.controller.ts
+++ Microservicios/appointment-service/src/appointment/appointment.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Post,
+} from '@nestjs/common';
+import { AppointmentService } from './appointment.service';
+import { CreateAppointmentDto } from './dto/createAppointment.dto';
+import { UpdateAppointmentDto } from './dto/updateAppointment.dto';
+import { Appointment } from './appointment.entity';
 
 @Controller('appointment')
-export class AppointmentController {}
+export class AppointmentController {
+  constructor(private readonly appointmentService: AppointmentService) { }
+
+  @Post()
+  async create(
+    @Body() createAppointmentDto: CreateAppointmentDto,
+  ): Promise<Appointment> {
+    return this.appointmentService.create(createAppointmentDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Appointment[]> {
+    return this.appointmentService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Appointment> {
+    return this.appointmentService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateAppointmentDto: UpdateAppointmentDto,
+  ): Promise<Appointment> {
+    return this.appointmentService.update(Number(id), updateAppointmentDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.appointmentService.remove(Number(id));
+  }
+}
