26f769b73eed91ff9abc3ced139dca8bbbcef78b
Bernapai <juanemeunier@gmail.com>
2025-04-30 17:24:28 -0300

    Add AuthController and Update authModule

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { loginUserDto } from './dtos/userLogin.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  async login(user: loginUserDto) {
+    return this.authService.login(user);
+  }
+}
--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../../../Microservicios/user-service/src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { User } from '../../../Microservicios/user-service/src/users/users.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Tipo de base de datos
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Juanber123()',
+      database: 'usersAdoptme',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    JwtModule.register({
+      secret: 'secreto',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
