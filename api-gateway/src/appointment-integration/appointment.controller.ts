f89a7b14d5fa9e4da9b08b0eb563681a07721a35
Bernapai <juanemeunier@gmail.com>
2025-05-04 17:28:05 -0300

    Add appointmentController from apigateway for integration

---
+++ api-gateway/src/appointment-integration/appointment.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { AppointmentsService } from './appointment.service';
+import { CreateAppointmentDto } from '../../../Microservicios/appointment-service/src/appointment/dto/createAppointment.dto';
+import { UpdateAppointmentDto } from '../../../Microservicios/appointment-service/src/appointment/dto/updateAppointment.dto';
+import { Appointment } from '../../../Microservicios/appointment-service/src/appointment/appointment.entity';
+
+@Controller('appointments')
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) { }
+
+  @Post()
+  create(@Body() dto: CreateAppointmentDto): Promise<Appointment> {
+    return this.appointmentsService.create(dto);
+  }
+
+  @Get()
+  findAll(): Promise<Appointment[]> {
+    return this.appointmentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Appointment> {
+    return this.appointmentsService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateAppointmentDto,
+  ): Promise<Appointment> {
+    return this.appointmentsService.update(Number(id), dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.appointmentsService.remove(Number(id));
+  }
+}
